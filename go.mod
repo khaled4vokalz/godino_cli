9fd11167c6740035bd09937b8d45942fe4970f84
Md. Mohiuddin Khaled Maruf <maruf.khaled@gmail.com>
2025-08-05 20:33:46 +0600

    Initial setup
    - just needed dirs and go files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cli-dino-game
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("CLI Dino Game")
+	fmt.Println("Press Ctrl+C to exit")
+	
+	// Basic application entry point
+	// Game initialization will be added in subsequent tasks
+	
+	// For now, just wait for user input to prevent immediate exit
+	fmt.Println("Game setup complete. Implementation coming soon...")
+	
+	// Exit gracefully
+	os.Exit(0)
+}
